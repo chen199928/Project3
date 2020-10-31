53142a960ff620bbcefcc444618cad5ac70d7913
chen199928 <57305176+chen199928@users.noreply.github.com>
2020-10-31 12:57:09 +0800

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,77 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Runner {
+    static final int NumRecs = 1024; // Each record holds 8 bytes. Each block has 8192 bytes
+    
+    /** Initialize the random variable */
+    static private Random value = new Random(); // Hold the Random class object
+
+        
+    static int randInt() {
+        return value.nextInt(Integer.MAX_VALUE );
+    }
+
+    static float randFloat() {
+        return value.nextFloat()*Float.MAX_VALUE;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        
+       DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream("Sampledata_16blocks.bin")));
+       RandomAccessFile rfile = new RandomAccessFile("Sampledata_16blocks.bin", "r"); 
+       //rfile.readLine();
+       int pointer = 0;
+//       byte[] intarray = new byte[4];
+//       byte[] floatarray = new byte[4];
+//       rfile.seek(0);
+//       rfile.read(intarray);
+//       System.out.print(convertByteArrayToInteger(intarray) + "    ");
+       while (pointer <= rfile.length() - 1) {
+           byte[] intarray = new byte[4];
+           byte[] floatarray = new byte[4];
+           rfile.seek(pointer);
+           rfile.read(intarray);
+           System.out.print(convertByteArrayToInteger(intarray) + "    ");
+           //break;
+           pointer = pointer + 4;
+           rfile.seek(pointer);
+           rfile.read(floatarray);
+           System.out.println(convertByteArrayToFloat(floatarray));
+           pointer = pointer + 4;
+       }
+    
+      
+ 
+    }
+    public static int convertByteArrayToInteger(byte[] intBytes)
+    {
+      ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.BYTES);
+      byteBuffer.put(intBytes);
+      byteBuffer.flip();
+      return byteBuffer.getInt();
+    }
+    
+    public static float convertByteArrayToFloat(byte[] floatBytes)
+    {
+      ByteBuffer byteBuffer = ByteBuffer.allocate(Float.BYTES);
+      byteBuffer.put(floatBytes);
+      byteBuffer.flip();
+      return byteBuffer.getFloat();
+    }
+}
+
