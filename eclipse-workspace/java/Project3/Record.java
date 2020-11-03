af8b942d4e163f0ed7ac3cc385438511b8de53ee
Youwei Chen <youweichen@192.168.2.29>
2020-11-03 12:12:24 +0800

    123

---
+++ eclipse-workspace/java/Project3/Record.java
@@ -0,0 +1,18 @@
+
+public class Record {
+    int key;
+    float value;
+    
+    public Record(int key, float value) {
+        this.key = key;
+        this.value = value;
+    }
+    
+    public int getKey() {
+        return key;
+    }
+    
+    public float value() {
+        return value;
+    }
+}
